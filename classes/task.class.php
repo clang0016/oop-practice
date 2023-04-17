9204bfed8e86887e9f243f6e8d8dd7596350d2c1
clang <130537679+clang0016@users.noreply.github.com>
2023-04-17 11:36:45 +0800

    first

---
+++ classes/task.class.php
@@ -0,0 +1,16 @@
+<?php
+class Task{
+    public $id;
+    public function show(){
+        echo 'show i2';
+    }
+
+    public function insert(){
+        echo 'insert i2';
+    }
+
+    public function delete(){
+        echo 'delete e2';
+    }
+}
+?>
